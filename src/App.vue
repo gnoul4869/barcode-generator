77944d657913b99e2f792cf30ce64854e6b759b3
gnoul_ <ttluong4869@gmail.com>
2022-08-06 16:15:50 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+    <header>
+        <h1>Barcode Generator</h1>
+    </header>
+
+    <main>
+        <hi>Hello world</hi>
+    </main>
+</template>
+
+<style scoped></style>
